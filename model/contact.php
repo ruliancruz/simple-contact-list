822dc8d64bab9efb2a9477996265a39594c5b9b0
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-09-29 03:54:29 -0300

    finish contact model

--- model/contact.php
+++ model/contact.php
@@ -1,24 +1,103 @@
 <?php
 class Contact
 {
-  private $atributes = [];
+  private $attributes = [];
 
   public function __construct() {}
 
   public function __set(string $atribute, $value)
   {
-    $this->atributes[$atribute] = $value;
-    return $this;
+    $this->attributes[$atribute] = $value;
   }
 
   public function __get(string $atribute)
   {
-    return $this->atributes[$atribute] ?? null;
+    return $this->attributes[$atribute] ?? null;
   }
 
   public function __isset($atribute)
   {
-    return isset($this->atributes[$atribute]);
+    return isset($this->attributes[$atribute]);
+  }
+
+  public static function all()
+  {
+    $sql = "SELECT * FROM contacts";
+    $contacts = array();
+
+    Database::useConnection(function($connection) use ($sql, &$contacts)
+    {
+      $result = $connection->query($sql);
+
+      while($contact = $result->fetchObject(Contact::class))
+      {
+        $contacts[] = $contact;
+      }
+    });
+
+    return $contacts;
+  }
+
+  public static function count()
+  {
+    $sql = "SELECT count(*) FROM contacts";
+    $count = null;
+
+    Database::useConnection(function($connection) use ($sql, &$count)
+    {
+      $count = $connection->query($sql)->fetchColumn();
+    });
+
+    return $count ? (int) $count : false;
+  }
+
+  public static function find($id)
+  {
+    $sql = "SELECT * FROM contacts WHERE id = :id";
+    $contact = false;
+
+    Database::useConnection(function($connection) use ($sql, $id, &$contact)
+    {
+      $statement = $connection->prepare($sql);
+      $statement->bindParam(':id', $id, PDO::PARAM_INT)->execute();
+
+      if($statement->rowCount() > 0)
+      {
+        $contact = $statement->fetchObject(Contact::class);
+      }
+    });
+
+    return $contact;
+  }
+
+  public static function destroy($id)
+  {
+    $sql = "DELETE FROM contacts WHERE id = :id";
+    $success = false;
+
+    Database::useConnection(function($connection) use ($sql, $id, &$success)
+    {
+      $statement = $connection->prepare($sql);
+      $success = $statement->bindParam(':id', $id, PDO::PARAM_INT)->execute();
+    });
+
+    return $success;
+  }
+
+  public function save()
+  {
+    $columns = $this->prepare($this->attributes);
+    $sql = $this->saveSQL($columns);
+    $count = false;
+
+    Database::useConnection(function($connection) use ($sql, &$count)
+    {
+      $statement = $connection->prepare($sql);
+
+      if($statement->execute()) { $count = $statement->rowCount(); }
+    });
+
+    return $count;
   }
 
   private function escape(string $data)
@@ -33,16 +112,46 @@ class Contact
 
     return 'NULL';
   }
-}
-
-private function prepare($data)
-{
-  $result = array();
-
-  foreach($data as $key => $value)
+  
+  private function prepare($data)
   {
-    if(is_scalar($value)) { $result[$key] = $this->escape($value); }
+    $result = array();
+
+    foreach($data as $key => $value)
+    {
+      if(is_scalar($value)) { $result[$key] = $this->escape($value); }
+    }
+
+    return $result;
   }
 
-  return $result;
+  private function saveSQL($columns)
+  {
+    if(!isset($this->id)) { return $this->createSQL($columns); }
+
+    return $this->updateSQL($columns);
+  }
+
+  private function createSQL($columns)
+  {
+    $sql = "INSERT INTO contacts (" .
+      implode(',', array_keys($columns)) .
+      ") VALUES (" .
+      implode(',', array_values($columns)) .
+      ")";
+
+    return $sql;
+  }
+
+  private function updateSQL($columns)
+  {
+    foreach($columns as $key => $value)
+    {
+      if($key !== 'id') { $set[] = "{$key}={$value}"; }
+    }
+
+    return "UPDATE contacts SET " .
+           implode(',', $set) .
+           " WHERE id={$this->id}";
+  }
 }
